13290b6efe7a9abd7c1a62424ccdafd7de961e88
bijokmathew <bijo.nbr@hotmail.com>
2022-11-30 01:28:15 +0000

    added game funcs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,56 @@
 let game = {
     score :0,
+    turnNumber:0,
     currentGame:[],
     playerMoves:[],
     choices:["button1","button2","button3","button4"],
 }
 
-module.exports={game};
+function newGame() {
+    game.score =0;
+    game.turnNumber=0;
+    game.currentGame=[];
+    game.playerMoves=[];
+    for(let circle of document.getElementsByClassName("circle")) {
+        if(circle.getAttribute("data-listener") === "false") {
+            circle.addEventListener("click",(e)=>{
+                let id = e.target.getAttribute("id");
+                lightsOn(id);
+                game.playerMoves.push(id);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener","true");
+        }
+    }
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves=[];
+    game.currentGame.push(game.choices[(Math.floor(Math.random()*4))])
+    showTurn();
+}
+
+function lightsOn(button){
+    document.getElementById(button).classList.add("light");
+    setTimeout(()=>{
+        document.getElementById(button).classList.remove("light");
+    },400);
+}
+
+function showTurn() {
+    game.turnNumber=0;
+    let intreval = setInterval(()=>{
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(intreval);
+        }
+    },800);
+}
+module.exports={game,newGame,showScore,addTurn,lightsOn,showTurn};
--- assets/test/game.test.js
+++ assets/test/game.test.js
@@ -1,5 +1,5 @@
 
-const {game} = require("../js/game");
+const {game,newGame,showScore,addTurn,lightsOn,showTurn} = require("../js/game");
 
 beforeAll(()=>{
     let fs = require("fs");
@@ -25,4 +25,76 @@ describe("game object contains correct key",()=>{
     test("choices contain correct ids",()=>{
         expect(game.choices).toEqual(["button1","button2","button3","button4"]);
     });
+    
+    test("turnNumber key exist",()=>{
+        expect("turnNumber" in game).toBe(true);
+    });
+});
+
+
+describe("newgame() works correctly",()=>{
+    beforeAll(()=>{
+        game.score =33;
+        game.currentGame =["button1","button3"];
+        game.playerMoves=["button4","button3"];
+        document.getElementById("score").innerText ="56";
+        newGame();
+    });
+
+    test("game score should set to zeor ",()=>{
+        expect(game.score).toEqual(0);
+    });
+
+    test("game currentGames should have one value",()=>{
+        expect(game.currentGame.length).toBe(1);
+    });
+
+    test("game playerMoves set to zero",()=>{
+        expect(game.playerMoves.length).toEqual(0);
+    });
+
+    test("should display 0 for the element with id of score",()=>{
+        expect(document.getElementById("score").innerText).toEqual(0)
+    });
+
+});
+
+describe("Game play works correctly",()=>{
+    beforeEach(()=>{
+        game.score=0;
+        game.currentGame=[];
+        game.playerMoves=[];
+        addTurn();
+    });
+    afterEach(()=>{
+        game.score=0;
+        game.currentGame=[];
+        game.playerMoves=[];
+    })
+    
+    test("addTurns add new turn to the game",()=>{
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+
+    test("should add correct class to light up buttons",()=>{
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+
+    test("showTurn should update game.turnNumber",()=>{
+        beforeAll(()=>{
+            game.turnNumber =42;
+        })
+        showTurn()
+        expect(game.turnNumber).toBe(0);
+    });
+   
+    test("All circles should attached with eventlistner",()=>{
+        circleList = document.getElementsByClassName("circle");
+        for(circle of circleList) {
+            expect(circle.getAttribute("data-listener")).toEqual("true");
+        }
+    })
 });
